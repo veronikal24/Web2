cd6faba296e93848385e5e1f55cb951d2222aca4
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 11:11:30 +0200

    ver

--- Assignment2/my-app/src/index.js
+++ Assignment2/my-app/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Root from "./routes/root";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} 
+from "react-router-dom";
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+     />
+         <RouterProvider router={router} />
   </React.StrictMode>
 );
 
