db2ae22f9d57489a3b88df96177e10993a341dab
Veronika Lietavcova <74155407+veronikal24@users.noreply.github.com>
2023-03-30 11:35:57 +0200

    git

--- Assignment2/my-app/src/index.js
+++ Assignment2/my-app/src/index.js
@@ -20,9 +20,10 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App
-     />
-         <RouterProvider router={router} />
+    <App>
+    <RouterProvider router={router} />
+    </App>
+  
   </React.StrictMode>
 );
 
